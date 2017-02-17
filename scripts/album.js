86ca976d94f76220c97dbf460de0d51057078e9d
Diego Aguilar <diegoaguilar@Diegos-MacBook-Pro-2.local>
2017-02-16 17:32:29 -0800

    checkpoint12: added play button and hover effect

--- scripts/album.js
+++ scripts/album.js
@@ -1,34 +1,34 @@
- var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21' },
-        { title: 'Magenta', duration: '2:15' },
-    ]
-  };
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
 
  var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
-    songs: [
-        { title: 'Hello, Operator?', duration: '1:01' },
-        { title: 'Ring, ring, ring', duration: '5:01' },
-        { title: 'Fits in your pocket', duration: '3:21' },
-        { title: 'Can you hear me now?', duration: '3:14' },
-        { title: 'Wong phone number?', duration: '2:15' },
-    ]
-  };
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
 
-  var albumDiego = {
+ var albumDiego = {
      title: 'How Bout Dat',
      artist: 'D-Train in the Mix',
      label: 'GL',
@@ -41,18 +41,19 @@
          { title: 'Hello Dear Friend', duration: '2:14' },
          { title: 'What up Kev?', duration: '5:15' },
      ]
-   };
+ };
 
  var createSongRow = function(songNumber, songName, songLength) {
-    var template =
+     var template =
         '<tr class="album-view-song-item">'
-      + '   <td class="song-item-number">' + songNumber + '</td>'
-      + '   <td class="song-item-title">' + songName + '</td>'
-      + '   <td class="song-item-duration">' + songLength + '</td>'
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
       ;
 
-      return template;
+     return template;
  };
 
  var setCurrentAlbum = function(album) {
@@ -78,11 +79,28 @@
       }
   };
 
+  var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+  var songRows = document.getElementsByClassName('album-view-song-item');
+  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
-     setCurrentAlbum(albumDiego);
-     random = [albumPicasso, albumMarconi, albumDiego];
-     index = 0;
-     albumImage = addEventListener("click", function() {
+     setCurrentAlbum(albumPicasso);
+     songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         if (event.target.parentElement.className === 'album-view-song-item') {
+             // Change the content from the number to the play button's HTML
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+     var random = [albumPicasso, albumMarconi, albumDiego];
+     var index = 1;
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     albumImage.addEventListener("click", function(event) {
        setCurrentAlbum(random[index]);
        index++;
        if (index == random.length) {
