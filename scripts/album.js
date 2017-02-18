f9c5926d5c27c3604ac2b8e082a2f1c2cbf0304e
Diego Aguilar <diegoaguilar@Diegos-MacBook-Pro-2.local>
2017-02-17 19:23:01 -0800

    assignment-13: check for parent

--- scripts/album.js
+++ scripts/album.js
@@ -79,12 +79,18 @@ var albumPicasso = {
   };
 
 var findParentByClassName = function(element, targetClass) {
-  if (element) {
+    if (element.parentElement != null) {
+      console.log('No parent found');
+    }
+    else if (element) {
         var currentParent = element.parentElement;
         while (currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
         return currentParent;
+    }
+    else {
+      console.log('No parent found with that class name');
     }
 };
 
